f39cb92fd75cf592f8495a368a4e5d206054f44e
Antonio Gómez <agomezgar@gmail.com>
2016-11-13 11:51:53 +0100

    Crea base de datos si no existe, y si existe la borra y la recrea

--- admin/index.php
+++ admin/index.php
@@ -21,6 +21,13 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+// Vaciar base de datos si existe
+$sql = "DROP DATABASE IF  EXISTS laforja;";
+if ($conn->query($sql) === TRUE) {
+    echo "Base de datos previa borrada";
+} else {
+    echo "Error borrando la base de datos previa: " . $conn->error;
+}
 
 // Create database
 $sql = "CREATE DATABASE IF NOT EXISTS laforja;";
